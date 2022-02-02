69eca18eaffc78c22f08d8d5ae10a538400e5679
jerry <jerry@haoxiamedia.com>
2022-02-02 14:41:18 +0800

    create ps before fast-fused start

--- shell/start.sh
+++ shell/start.sh
@@ -7,7 +7,9 @@ FUSE_CONF_PATH="/etc/fastcfs/fcfs/"
 
 
 operate_mode=start
-fs_serverd ${STORE_CONF_PATH}server.conf $operate_mode
 fdir_serverd ${FDIR_CONF_PATH}server.conf $operate_mode
+fs_serverd ${STORE_CONF_PATH}server.conf $operate_mode
 fcfs_authd  ${AUTH_CONF_PATH}server.conf $operate_mode
+fcfs_pool create fs unlimited
+fcfs_pool grant admin fs
 fcfs_fused -N ${FUSE_CONF_PATH}fuse.conf $operate_mode
